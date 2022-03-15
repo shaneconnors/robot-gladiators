5b8fafff409a063c54ed6bb7b9d890c033dd862c
Shane Connors <shaneconnors1992@gmail.com>
2022-03-14 17:21:19 -0800

    checking debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -172,17 +172,15 @@ var shop = function() {
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
   );
  // use switch case to carry out action
+ debugger;
  switch (shopOptionPrompt) {
-  case 'REFILL':
-  case 'refill':
+  case 1:
     playerInfo.refillHealth();
     break;
-  case 'UPGRADE':
-  case 'upgrade':
+  case 2:
     playerInfo.upgradeAttack();
     break;
-  case 'LEAVE':
-  case 'leave':
+  case 3:
       window.alert("Leaving the store.");
       break;
     default:
